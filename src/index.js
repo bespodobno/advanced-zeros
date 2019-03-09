21519d97aa626a2b5a8076a209ed5183e5711581
bespodobno <nadzeya.tron@gmail.com>
2019-03-10 01:11:29 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,57 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+ 
+  let primeFactors =[];
+  primeFactors = findPrimeFactors (base);
+  let max = Math.max.apply(null, primeFactors);
+  let n = findLargestpower(number, max);
+  let count = calculateRepetitions(primeFactors,max);
+  let res =  Math.floor(n/count);
+  return res;
 }
+function calculateRepetitions(primeFactors,max){
+  let count=0;
+
+  for( let i=0; i< primeFactors.length; i++){
+     if( max == primeFactors[i] ) count++;
+    }
+  return count;
+}
+
+//найти max prime factor!!! 
+//let max = Math.max.apply(null, primeFactors); 
+
+function findPrimeFactors (num) {
+
+  let primeFactors = [];
+  while (num % 2 === 0) {
+      primeFactors.push(2);
+      num = num / 2;
+  }
+  
+  let sqrtNum = Math.sqrt(num);
+  for (let i = 3; i <= sqrtNum; i++) {
+      while (num % i === 0) {
+          primeFactors.push(i);
+          num = num / i;
+      }
+  }
+  
+  if (num > 2) {
+      primeFactors.push(num);
+  }
+  return primeFactors;
+}
+function  findLargestpower(n, p) 
+    { 
+        // Initialize result 
+        let ans = 0; 
+  
+        // Calculate x = n/p + n/(p^2) + n/(p^3) + .... 
+        while (n > 0) 
+        { 
+            n =Math.floor(n/p); 
+
+            ans +=  Math.floor(n); 
+        } 
+        return ans; 
+    } 
